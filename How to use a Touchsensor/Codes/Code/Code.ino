db8ab5c731b29be2ea84f7c8bff7da6c1081879c
AKarduinoTech <102293424+AKarduinoTech@users.noreply.github.com>
2023-05-18 16:48:38 +0530

    Updated the Code.ino

--- How to use a Touchsensor/Codes/Code/Code.ino
+++ How to use a Touchsensor/Codes/Code/Code.ino
@@ -4,6 +4,7 @@
 * ----------
 * Arduino uno, breadboard, touchsensor and jumper wires
 * 
+* This sketch is created by Anuj Karnik.
 ********************************************************************************************************************/
  
 int touchsensor = 8;
