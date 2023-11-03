c7f15e08119b3ce62e40c216f9b652e2828a3ba0
AKarduinoTech <102293424+AKarduinoTech@users.noreply.github.com>
2023-11-03 17:30:23 +0530

    Added wiring diagram and sketch

---
+++ How to use the HC SR04 sensor/code/code.ino
@@ -0,0 +1,36 @@
+/*This code shows how to use HC SR04 sensor with Arduino
+* 
+* Components
+* ----------
+* Arduino uno, HC-SR04, breadboard and some jumper cables.
+* 
+**********************************************************************************************************************************************/
+
+#include <Wire.h>
+
+const int trigPin = 9;  // Declaring pins
+const int echoPin = 10;
+
+long duration;  // Creating variables
+int distance;
+
+void setup() {
+
+    pinMode(trigPin, OUTPUT); // Declares trigPin as an output
+    pinMode(echoPin, INPUT); // Declares echoPin as an input
+
+    Serial.begin(9600);
+}
+
+void loop() {
+
+  digitalWrite(trigPin, HIGH);  // Turns on the trigPin for 10 micro seconds
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);  // Reads the echoPin, returns the sound wave travel time in microseconds
+
+  distance = duration * 0.0343 / 2;  // Calculating the distance
+  
+                                                                                                                                                                                                                                                                                                                                     
+}
